21230825af291f8547356660e9a3b3834db9a09d
eoen <info@eoen.org>
2015-09-06 08:18:36 +0800

    http://v2.youbbs.eoen.org/topics/24

--- templates/default/layout.php
+++ templates/default/layout.php
@@ -55,7 +55,7 @@ echo '
 				var dispatch = function() {
 					q = document.getElementById("q");
 					if (q.value != "" && q.value != "站内搜索") {
-						window.open(\'http://www.iglgoo.com/search?q=site:',$_SERVER['HTTP_HOST'],'%20\' + q.value, "_blank");
+						window.open(\'https://www.nssjs.com/search?q=site:',$_SERVER['HTTP_HOST'],'%20\' + q.value, "_blank");
 						return false;
 					} else {
 						return false;
